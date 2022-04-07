cd157f6b1b09c64e2da2fb69448e2039cd7a1d61
JoaoGeureri <odinpro5981@gmail.com>
2022-04-07 16:15:02 -0500

    Setup server and database

--- 1_instaverse/server/index.js
+++ 1_instaverse/server/index.js
@@ -2,3 +2,22 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "32mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "32mb", extended: true }));
+app.use(cors());
+
+// In a real app, you should not have your username and password like this
+const CONNECTION_URL =
+  "mongodb+srv://instaverse:MqNl93X04ftbxXSG@cluster0.hvhua.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((ERR) => console.log(err.message));
